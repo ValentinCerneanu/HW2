bfd2ffe0f300fa3fc984dc657ffc1ca6d56bbd6d
ValentinCerneanu <valentincerneanu28@gmail.com>
2020-01-09 00:01:30 +0200

    iterative dfs

--- Compiler/src/compiler/Compiler.java
+++ Compiler/src/compiler/Compiler.java
@@ -12,6 +12,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  *
@@ -24,7 +25,8 @@ public class Compiler {
     private static HashMap<String, Integer> classNames = new HashMap<String, Integer>();
     private static Integer n;
 
-    static int visited[] ;
+    static int visited[];
+    static int max;
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Scanner fileIn = new Scanner(new File("compiler.in"));
@@ -46,19 +48,28 @@ public class Compiler {
         FileWriter fileWriter = new FileWriter("compiler.out");
         PrintWriter printWriter = new PrintWriter(fileWriter);
         
+        int maxGlobal = 0;
+        boolean isCycle = false;
         visited = new int[n+1];
-        if(isCyclic(1))
-            printWriter.println(cycleDetected); 
-        else
-        {
-            int max = 0;
-            for(int i=1; i<n+1; i++){
-                if(max<visited[i])
-                    max = visited[i];
+        
+        for(int i=1; i<=n; i++){
+            if(isCyclic(i)){
+                printWriter.println(cycleDetected); 
+                isCycle = true;
+                break;
             }
-            printWriter.println(max+2);
+            else
+            {
+                if(maxGlobal < max+2){
+                    maxGlobal = max+2;
+                }
+            }
+        }
+        if(!isCycle){
+            printWriter.print(maxGlobal);
         }
         printWriter.close();
+
     }
     private static boolean isCyclic(int v) {
         
@@ -66,27 +77,65 @@ public class Compiler {
         
         for(int i=0; i<verticesAdjacentToV.size(); i++) {
             if(visited[verticesAdjacentToV.get(i)] == 0)
-                if(isCyclicUtil(verticesAdjacentToV.get(i), -1))
+                if(isCyclicComputer(verticesAdjacentToV.get(i), -1))
                     return true;
         }
         return false;
     }
     
-    private static boolean isCyclicUtil(int v, int parent){
-        if(parent == -1)
-            visited[v] = 1; 
-        else
-            visited[v] = visited[parent] + 1;
+    private static boolean isCyclicComputer(int v, int parent){
+        Stack<NodeAndParent> stack = new Stack<NodeAndParent>();
+        stack.push(new NodeAndParent(v, parent));
         
-        for(int i=0; i<graph.get(v).size(); i++) {
-            if(visited[graph.get(v).get(i)] == 0){
-                int vv = graph.get(v).get(i);
-                if(isCyclicUtil(vv, v))
-                    return true;
-            } else if (i != parent) 
-                return true; 
+        while (!stack.empty()) {
+            NodeAndParent nodeAndParent = stack.peek();
+            int node = nodeAndParent.node;
+            stack.pop();
+            parent = nodeAndParent.parent;
+            
+            if(parent == -1)
+                visited[node] = 1; 
+            else
+                visited[node] = visited[parent] + 1;
+            if(visited[node] > max){
+                max = visited[node];
+            }
+            
+            for(int i=0; i<graph.get(node).size(); i++) {
+                if(visited[graph.get(node).get(i)] == 0){
+                    int vv = graph.get(node).get(i);
+                    NodeAndParent nodeAndParentToStack = new NodeAndParent(vv, node);
+                    stack.push(nodeAndParentToStack);
+               }
+               else if (i != parent) 
+                   return true; 
+           }   
         }
-        
         return false;
+    }
+    
+    private static class NodeAndParent{
+        private int node;
+        private int parent;
+        public NodeAndParent(int node, int parent){
+            this.node = node;
+            this.parent = parent;
+        }
+
+        public int getNode() {
+            return node;
+        }
+
+        public void setNode(int node) {
+            this.node = node;
+        }
+
+        public int getParent() {
+            return parent;
+        }
+
+        public void setParent(int parent) {
+            this.parent = parent;
+        }
     }
 }
